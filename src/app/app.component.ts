97aedb7cd9015834314990d263ac26dc053d9163
Tawatchai Suktrakul <Tawatchai.Suktrakul@gmail.com>
2022-05-17 11:21:15 +0700

    Complete Example Section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fb-menu';
+  menuElements: { type: string, name: string, content: string }[] = [ {
+    type: "Food",
+    name: "Test",
+    content: "Some content"
+  }];
+  newMenuName = '';
+  newMenuContent = '';
+
+  constructor() {
+
+  }
+
+  onAddFood() {
+    this.menuElements.push({
+      type: 'Food',
+      name: this.newMenuName,
+      content: this.newMenuContent
+    });
+  }
+
+  onAddBeverage() {
+    this.menuElements.push({
+      type: 'Beverage',
+      name: this.newMenuName,
+      content: this.newMenuContent
+    });
+  }
+
+
+
+
+
 }
